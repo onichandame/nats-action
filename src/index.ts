31d1358944fabf443d1ceb1345306f101fe8a067
onichandame <zxinmyth@gmail.com>
2020-06-08 10:20:51 +0000

    test

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,38 @@
-import { info, setFailed, getInput } from '@actions/core';
-import { exec } from 'child_process';
+import { info, setFailed, getInput } from "@actions/core"
+import { exec } from "@actions/exec"
 
 const parsePorts = () => {
-  return getInput('port')
-    .split(' ')
+  return getInput("port")
+    .split(" ")
     .map(v => parseInt(v))
-    .filter(v => !isNaN(v) && v >= 0);
-};
+    .filter(v => !isNaN(v) && v >= 0)
+}
 
-const startServer = (
+const startServer = async (
   port: number,
   clusterPort?: number,
   routePort?: number,
   masterName?: string
 ) => {
-  let options: string[] = [];
-  options.push(...['-d', '-p', `${port}:${port}`]);
+  let options: string[] = []
+  options.push(...["-d", "-p", `${port}:${port}`])
   if (clusterPort)
-    options.push(...['--cluster', `nats://0.0.0.0:${clusterPort}`]);
+    options.push(...["--cluster", `nats://0.0.0.0:${clusterPort}`])
   if (routePort && masterName)
-    options.push(...['--routes', `nats://${masterName}:${routePort}`]);
-  options.push('nats');
-  options = ['sudo', 'docker', 'run'].concat(options);
-  exec(options.join(' '), e => {
-    if (e) setFailed(`failed to start server due to ${JSON.stringify(e)}`);
-  });
-};
-
-const ports = parsePorts();
-
-if (!ports.length) setFailed(`require at least 1 port`);
-
-info(`ports ${ports.join(', ')} will be used`);
-
-if (ports.length === 1) {
-  startServer(ports[0]);
-} else {
+    options.push(...["--routes", `nats://${masterName}:${routePort}`])
+  options.push("nats")
+  options = ["docker", "run"].concat(options)
+  if ((await exec("sudo", options)) !== 0) setFailed(`failed to start server`)
 }
+;(async () => {
+  const ports = parsePorts()
+
+  if (!ports.length) setFailed(`require at least 1 port`)
+
+  info(`ports ${ports.join(", ")} will be used`)
+
+  if (ports.length === 1) {
+    await startServer(ports[0])
+  } else {
+  }
+})()
